8b93f116842003b6d3773ede5d6d67d1c75b5232
Sourish Roy <sourish.roy1973@gmail.com>
2024-11-19 09:55:56 +0000

    UserModel, register user, jwt

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const connectDB = require("./db/connect");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(morgan("tiny"));
@@ -16,6 +18,8 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("ECommerce-API");
 });
+
+app.use("/api/v1/auth", authRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+const User = require("../models/User");
+const { createJWT, isTokenValid } = require("../utils");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // first registerd user is an admin
+  const isFirstAcc = (await User.countDocuments({})) === 0;
+  const role = isFirstAcc ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+};
+
+const login = async (req, res) => {
+  res.send("login user");
+};
+
+const logout = async (req, res) => {
+  res.send("logout user");
+};
+
+module.exports = { register, login, logout };
