d4fa82c6c90828925ba383f105fb28eef5de57f4
Sourish Roy <sourish.roy1973@gmail.com>
2024-11-26 13:23:00 +0000

    order model, controller done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -28,6 +29,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/reviews", reviewRoutes);
+app.use("/api/v1/orders", orderRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -2,14 +2,14 @@ const CustomError = require("../errors");
 
 /**
  * 
- * @param {*} requestUser userId associated with req.user
+ * @param {*} requestUser user obj associated with req.user
  * @param {*} resourceUserId userID associate with the specific resource
  * @returns 
  */
 const checkPermissions = (requestUser, resourceUserId) => {
-  // console.log(requestUser);
-  // console.log(resourceUserId);
-  // console.log(typeof requestUser);
+  console.log(requestUser);
+  console.log(resourceUserId.toString());
+  console.log(requestUser.role);
   if (requestUser.role === "admin") return;
   if (requestUser.userId === resourceUserId.toString()) return;
   throw new CustomError.UnauthorizedError(
